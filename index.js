1292b299505c83ec1f9c305745d8d591c7ed091c
SabbirSami <smd71430@gmail.com>
2022-09-03 11:53:28 +0600

    route created

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const userRoutes = require("./routes/users.route");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("ACC Assignment");
