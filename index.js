39bc7ebf1906126a1128aef08ec44131f588a20a
SabbirSami <smd71430@gmail.com>
2022-09-03 18:13:07 +0600

    get all users

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const userRoutes = require("./routes/users.route");
+const fs = require("fs");
 
 app.use(express.json());
 
-app.use("/users", userRoutes);
+app.get("/user/all", (req, res) => {
+    const allUser = fs.readFileSync("./userData.json");
+    const users = JSON.parse(allUser);
+    res.status(200).send(users);
+});
+// app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("ACC Assignment");
